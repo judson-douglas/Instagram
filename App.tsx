d61c02c461d3e904f5f96aab2e385b2322e4cca1
owner <owner@users-MacBook-Pro.local>
2023-09-15 12:42:14 -0500

    initialize project & add vector icons to ios

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import {View, Text} from 'react-native';
+import colors from './src/theme/colors';
+import font from './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text style={{color: colors.primary, fontSize: font.size.xlg}}>
+        Hello World
+        <AntDesign name="stepforward" size={25} />
+      </Text>
+    </View>
+  );
+};
+
+export default App;
